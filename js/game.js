b3bc28e0e9309f1e667e911828d9d00df86612cc
Bosbobos <konovalov.matvej@mail.ru>
2020-03-01 17:24:30 +0300

    Ыыыыы

--- js/game.js
+++ js/game.js
@@ -13,10 +13,15 @@ fg.src = "img/fg.png";
 pipeUp.src = "img/pipeUp.png"; 
 pipeBottom.src = "img/pipeBottom.png"; 
 
+var gap = 90;
+
 function draw() {
     ctx.drawImage(bg, 0, 0);
 
     ctx.drawImage(pipeUp, 100, 0)
+    ctx.drawImage(pipeBottom, 100, 0 + pipeUp.height + gap);
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
 }
 
 pipeBottom,onload = draw;
